d0b43b214921e07c03f794f6e98bd41614c27f00
Shubham Kothari <shubham@noborders.net>
2021-02-10 19:46:26 +0530

    Before Test Flight build shared

--- src/screens/AddFriendByQr/index.js
+++ src/screens/AddFriendByQr/index.js
@@ -186,7 +186,6 @@ class AddFriendByQr extends Component {
                                     </TouchableOpacity>
                                 </ImageBackground>
                                 : null
-
                         }
                     />
                 }
